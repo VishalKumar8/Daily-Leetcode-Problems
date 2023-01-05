a758f2e919ff39f202ba8d8c979025a8fbe0620d
Vishal Kumar <88970099+VishalKumar8@users.noreply.github.com>
2023-01-05 20:36:41 +0530

    Update 452. Minimum Number of Arrows to Burst Balloons.cpp

--- January/452. Minimum Number of Arrows to Burst Balloons.cpp
+++ January/452. Minimum Number of Arrows to Burst Balloons.cpp
@@ -2,12 +2,19 @@
 
 Approach:
 
-We can sort the array based on starting or ending points, lets go with ending points.
-Lets observe the given test case
-image
-Here we can see that [1,6] and [2,8] overlap because starting of [2,8] is less than ending of [1,6]. In such cases 1 arrow will burst them both.
-But if we check [1,6] and [7,12], they do not overlap because starting of [7,12] is not less than ending of [1,6]. In such cases, another arrow will be needed to burst it.
-We will just apply this logic in our solution.  */
+=> We can sort the array based on starting or ending points, lets go with ending points.
+=> Lets observe the given test case
+
+=> input: points = [[10,16],[2,8],[1,6],[7,12]]
+   output: 2
+   Explanation: The balloons can be burst by 2 arrows: 
+   - Shoot an arrow at x = 6, bursting the balloons [2,8] and [1,6].
+   - Shoot an arrow at x = 11, bursting the balloons [10,16] and [7,12].
+   
+=> Here we can see that [1,6] and [2,8] overlap because starting of [2,8] is less than ending of [1,6]. In such cases 1 arrow will burst them both.
+=> But if we check [1,6] and [7,12], they do not overlap because starting of [7,12] is not less than ending of [1,6]. In such cases, another arrow will be needed to burst it.
+
+=> We will just apply this logic in our solution.  */
 
 
 /* Time Complexity => O(NlogN)

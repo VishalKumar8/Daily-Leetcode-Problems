caa8c12f406fdd70719818f82d822f55b0d7baed
Vishal Kumar <88970099+VishalKumar8@users.noreply.github.com>
2023-03-03 10:13:41 +0530

    Update Find the Index of the First Occurrence in a String.cpp

--- 03. March 2023/Find the Index of the First Occurrence in a String.cpp
+++ 03. March 2023/Find the Index of the First Occurrence in a String.cpp
@@ -1,92 +1,30 @@
 /*
- * @lc app=leetcode id=28 lang=cpp
- *
- * [28] Implement strStr()
- *
- * https://leetcode.com/problems/implement-strstr/description/
- *
- * algorithms
- * Easy (34.96%)
- * Likes:    2066
- * Dislikes: 2183
- * Total Accepted:    792.2K
- * Total Submissions: 2.3M
- * Testcase Example:  '"hello"\n"ll"'
- *
- * Implement strStr().
- * 
- * Return the index of the first occurrence of needle in haystack, or -1 if
- * needle is not part of haystack.
- * 
- * Clarification:
- * 
- * What should we return when needle is an empty string? This is a great
- * question to ask during an interview.
- * 
- * For the purpose of this problem, we will return 0 when needle is an empty
- * string. This is consistent to C's strstr() and Java's indexOf().
- * 
- * 
- * Example 1:
- * Input: haystack = "hello", needle = "ll"
- * Output: 2
- * Example 2:
- * Input: haystack = "aaaaa", needle = "bba"
- * Output: -1
- * Example 3:
- * Input: haystack = "", needle = ""
- * Output: 0
- * 
- * 
- * Constraints:
- * 
- * 
- * 0 <= haystack.length, needle.length <= 5 * 10^4
- * haystack and needle consist of only lower-case English characters.
- * 
- * 
- */
+Link to the " Find the Index of the First Occurrence in a String " Problem ==>>  https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/ 
+   
+Link to the complete Explaination Video ==>> https://youtu.be/MIu2o3Wrlg0
+*/
 
-#include<bits/stdc++.h>
-using namespace std;
-
-// @lc code=start
 class Solution {
 public:
-    int strStr(string haystack, string needle) {
-        int nh=haystack.length();
-        int nn=needle.length();
-        if(nn==0){
-            return 0;
-        }
-        if(nn==1){
-            for(int i=0;i<nh;i++){
-                if(haystack[i]==needle[0]){
-                    return i;
-                }
-            }
+    int strStr(string h, string n) {
+
+        if(h.size()<n.size()){
             return -1;
         }
-        // we only need to go upto (len of haystack - len of needle)
-        for(int i=0;i<nh-nn+1;i++){
-            // may be possible
-            // check if (curr i + len of needle) == end char of needle
-            int check=i+nn-1;
-            if(haystack[i]==needle[0]&&haystack[check]==needle[nn-1]){
-                int x=1;
-                for(int j=i+1;j<check&&j<nh;j++){
-                    if(haystack[j]!=needle[x]){
-                        break;
-                    }else{
-                        x++;
-                    }
+        int j=0;
+        for(int i=0;i<h.size()-n.size()+1;i++){
+            if(h[i]==n[j]){
+                int l=i;
+                while(j<n.size() && h[l]==n[j]){
+                    l++;
+                    j++;
                 }
-                if(x==nn-1){
+                if(j==n.size()){
                     return i;
                 }
             }
+            j=0;
         }
         return -1;
     }
 };
-// @lc code=end

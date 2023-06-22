2430af086d5e274743e0b464ad4ad563177bfd2d
Vishal Kumar <88970099+VishalKumar8@users.noreply.github.com>
2023-06-22 22:48:40 +0530

    Update Best Time to Buy and Sell Stock with Transaction Fee.cpp

--- 06. June 2023/Best Time to Buy and Sell Stock with Transaction Fee.cpp
+++ 06. June 2023/Best Time to Buy and Sell Stock with Transaction Fee.cpp
@@ -1,1 +1,13 @@
-
+class Solution {
+public:
+    int maxProfit(vector<int>& prices, int fee) {
+        int n = prices.size();
+        int buy = -prices[0], sell = 0;
+        for(int i=0; i<n; i++){
+            int temp = buy;
+            buy = max(buy, sell - prices[i]);
+            sell = max(sell, temp+prices[i] - fee);
+        }
+        return sell;
+    }
+};
